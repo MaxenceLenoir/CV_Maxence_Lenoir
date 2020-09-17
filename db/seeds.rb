e1f8b64118ac33d5a297179ab31979cbe0617043
maxencelenoir <maxence.lenoir1206@gmail.com>
2020-09-17 11:52:51 +0200

    Ajout projet Gratte Ton Chomage

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,7 @@ puts "Creating some projects ..."
 
 Project.create!(name: "AirbnBike", url: "https://airbnbikes.herokuapp.com/", skill_one: "Airbnb like - Market place", skill_two: "Système de paiement - Stripe", skill_three: "Géomapping & géolocalisation - Mapbox", description: "Market place permettant de mettre en relation des cyclistes occasionnels.", client: "1er projet du Wagon", asset_url: 'AirbnBike.JPG')
 Project.create!(name: "HomeBoard", url: " https://mynewhomeboard.herokuapp.com/", skill_one: "Chat live - Action Cable", skill_two: "Notifications en temps réel - Action Cable", skill_three: "Authentification & Authorization",  description: "Application qui permet de gérer sa vie de propriétaire et améliorer les relations de voisinages au travers de services", client: "Projet final du Wagon", asset_url: 'Homeboard.JPG')
+Project.create!(name: "GratteTonChomage", url: "http://www.grattetonchomage.fr/", skill_one: "Calcul allocation - Back-end", skill_two: "Export PDF - Gem wicked-pdf", skill_three: "formulaire AJAX gestions erreurs", description: "Application qui permet de faciliter l'accès au chomage pour les personnes revenant de V.I", client: "projet perso", asset_url: 'Grattetonchomage.JPG')
 
 puts "Finished !"
 
